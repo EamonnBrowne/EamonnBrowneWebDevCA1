c79aa7b05cca7b66c38cd300eecf768f785bc164
EamonnBrowne <x23309709@student.ncirl.ie>
2024-11-04 13:11:09 +0000

    First commit of CA1 Code

---
+++ Scripts/Script.js
@@ -0,0 +1,16 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  // Prevent the form from submitting immediately
+  event.preventDefault();
+  /* read the Email value from form with id contactForm */
+  let fEmail = document.forms["contactForm"]["Email"].value;
+  /* use a regular expression to validate email addess is in the form someone@somewhere.something */
+  let re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!re.test(fEmail)){
+    alert("Email is in an invalid format");
+  }
+  else {
+    /* Display a message to say the form has been submitted */
+    alert ("Form Successfully submitted");
+  }
+});
